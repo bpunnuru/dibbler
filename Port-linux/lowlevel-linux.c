d6110d04129374032d10d83995762b74d6dbebe8
Damian Manelski <dm1988air@gmail.com>
2013-10-23 19:48:39 +0200

    warnings cleaning,problem with CurrentMsg

--- Port-linux/lowlevel-linux.c
+++ Port-linux/lowlevel-linux.c
@@ -830,7 +830,7 @@ extern int sock_add_tcp (char * ifacename,int ifaceid, char * addr, int port) {
     struct addrinfo *res, *rp;
     struct addrinfo hints;
     fd_set master_set;
-    int Insock;
+    int Insock=0;
     char port_char[6];
     char * tmp;
     int connectionNumber =1;
@@ -1001,7 +1001,8 @@ extern int sock_add_tcp (char * ifacename,int ifaceid, char * addr, int port) {
 //int getsockopt(int socket, int level, int option_name,void *restrict option_value, socklen_t *restrict option_len);
 extern int getsOpt(int fd) {
 
-    int len, sockType, result;
+    int  sockType, result;
+    socklen_t len;
 
     len = sizeof(sockType);
     result = getsockopt(fd,SOL_SOCKET,SO_TYPE,&sockType,&len);
@@ -1021,7 +1022,7 @@ extern int accept_tcp (int fd, char *peerPlainAddr) {
     int fd_new;
 
     bzero(&peerStructure, sizeof(struct sockaddr_in6));
-    int peerStructureLen = sizeof(peerStructure);
+    socklen_t peerStructureLen = sizeof(peerStructure);
 
     fd_new = accept(fd,(struct sockaddr_in6 *)&peerStructure,&peerStructureLen);
     if (fd_new < 0) {
